a5abe401384a9ef8a97977011c6dcb282fd2dd2e
Joker <mandresiarivonyandriniaina@gmail.com>
2026-02-04 21:02:56 +0300

    Ajout du point d’entrée JavaFX (Main)

--- src/main/java/com/browser/Main.java
+++ src/main/java/com/browser/Main.java
@@ -1,17 +1,1 @@
-package main.java.com.browser;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-
-public class Main extends Application {
-
-    @Override
-    public void start(Stage stage) {
-        stage.setTitle("Browser Java Socket");
-        stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-}
