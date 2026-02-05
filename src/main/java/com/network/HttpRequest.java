35c60cc87f3cd8a843dd39e8e3d870925156c898
Joker <mandresiarivonyandriniaina@gmail.com>
2026-02-05 12:58:32 +0300

    Ajout du fichier requete HTTP

---
+++ src/main/java/com/network/HttpRequest.java
@@ -0,0 +1,37 @@
+package com.browser.network;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpRequest {
+    private String methode;
+    private String url;
+    private Map<String, String> entetes;
+    private String corps;
+
+    public HttpRequest(String methode, String url) {
+        this.methode = methode;
+        this.url = url;
+        this.entetes = new HashMap<>();
+        this.corps = "";
+        // Entêtes par défaut pour un navigateur
+        this.entetes.put("User-Agent", "JavaBrowser/1.0");
+        this.entetes.put("Accept", "text/html");
+        this.entetes.put("Connection", "close");
+    }
+
+    public void ajouterEntete(String cle, String valeur) {
+        this.entetes.put(cle, valeur);
+    }
+
+    public String genererRequete() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(methode).append(" ").append(url).append(" HTTP/1.1\r\n");
+        for (Map.Entry<String, String> entree : entetes.entrySet()) {
+            sb.append(entree.getKey()).append(": ").append(entree.getValue()).append("\r\n");
+        }
+        sb.append("\r\n");
+        sb.append(corps);
+        return sb.toString();
+    }
+}
