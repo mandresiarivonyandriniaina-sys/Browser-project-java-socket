d7ebe63496b64dcb419b5a02f8b8da987339c39a
Joker <mandresiarivonyandriniaina@gmail.com>
2026-02-05 22:25:16 +0300

    petit code pour tester JavaFX

--- src/main/java/com/browser/Main.java
+++ src/main/java/com/browser/Main.java
@@ -1,11 +1,18 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
         stage.setTitle("JavaFX OK");
+        Label label = new Label("JavaFX marche");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 300, 200);
+        stage.setScene(scene);
         stage.show();
     }
 
